7ed59097920af503be7b9b909ce9da2445fb77b6
unknown <skipperstrange@gmail.com>
2022-09-13 23:27:41 +0200

    Porto compulsory stylesheets added to resources

--- Views/layouts/_header.php
+++ Views/layouts/_header.php
@@ -2,7 +2,8 @@
 <html>
 <head>
     <!-- Required meta tags -->
-    <?= generate_tag_group('meta',$metadata) ?>
+    <?= generate_tag_group('meta',$metadata, false) ?>
+    <?= generate_tag_group('link',$styles, false) ?>
     <title><?= _title($pageTitle) ?></title>
 </head>
 
